08dd23eb13cf5c705d95ff0cc634c2fd1486f345
Arman Arif <arman3472rmn@gmail.com>
2022-04-12 02:41:45 +0600

    add: modal component

---
+++ src/components/Modal.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { Button, Modal as BsModal } from "react-bootstrap";
+
+function Modal(props) {
+    const [show, setShow] = useState(true);
+
+    function closeHandler() {
+        setShow(false);
+        props.onClose();
+    }
+
+    return (
+        <BsModal show={show}>
+            <BsModal.Body className="pt-3">
+                <h4 className="text-center mb-4">Are you sure?</h4>
+                <div className="text-center">
+                    <Button className="mx-3" variant="outline-secondary" onClick={closeHandler}>
+                        Close
+                    </Button>
+                    <Button className="mx-3" variant="danger" onClick={closeHandler}>
+                        Confirm
+                    </Button>
+                </div>
+            </BsModal.Body>
+        </BsModal>
+    );
+}
+
+export default Modal;
