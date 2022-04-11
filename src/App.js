79867961ff0d64d334cffb32d8b7fdc222bf5c97
Arman Arif <arman3472rmn@gmail.com>
2022-04-12 02:42:24 +0600

    add: front design and dummy todo data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { Container, Navbar, Row, Col } from "react-bootstrap";
+import Composer from "./components/Composer";
+import Todo from "./components/Todo";
 
 function App() {
 
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Learn React",
+      done: false
+    },
+    {
+      id: 2,
+      title: "Learn Vue",
+      done: true
+    },
+    {
+      id: 3,
+      title: "Learn Angular",
+      done: false
+    }
+  ]);
 
     return (
-       <div>Hello world</div>
+        <Container fluid className="p-0 bg-light vh-100">
+            <Navbar bg="dark" expand="lg" className="justify-content-center">
+                <Navbar.Brand>
+                    <h1 className="text-white">Todos</h1>
+                </Navbar.Brand>
+            </Navbar>
+            <Container className="p-3">
+                <Row className="justify-content-center">
+                    <Col md={10} lg={8}>
+                        <Composer/>
+                        <Row>
+                          {todos.sort((a, b) => a.done > b.done ? 1 : -1).map(todo => <Todo task={todo} key={todo.id}/>) }
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        </Container>
     );
 }
 
